0eb1ae437093f8a089d79237121ab91c496bdf48
ag3ntk1wi <jmalate019@gmail.com>
2024-09-18 13:29:59 +0800

    commit 1

---
+++ PS03_LoopYear/PS03_LoopYear/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS03_LoopYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a function that determines whether a given year is a leap year or not.
+
+            void LoopYear(int y)
+            {
+                if (y % 4 == 0)
+                {
+                    if (y % 100 == 0 && y % 400 != 0)
+                    {
+                        Console.WriteLine("Not a Loop Year.");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Loop Year.");
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Not a Loop Year.");
+                }
+            }
+
+            Console.Write("Enter a Year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            LoopYear(year);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PS03_Malate/PS03_Malate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS03_Malate
+{
+    public class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            //Create a function that computes the area of a triangle using Heron’s formula.
+
+            double HeronFormula(double s, double a, double b, double c)
+            {
+                return Math.Sqrt(s*(s-a)*(s-b)*(s-c));
+            }
+
+            Console.Write("Enter Length of side A: ");
+            double lengthA = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Length of side B: ");
+            double lengthB = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Length of side C: ");
+            double lengthC = Convert.ToDouble(Console.ReadLine());
+
+            double semiPerimeter = (lengthA + lengthB + lengthC) / 2;
+            double area = HeronFormula(semiPerimeter, lengthA, lengthB, lengthC);
+
+            Console.WriteLine("The Semi-Parameter is " + semiPerimeter);
+            Console.WriteLine("The area of the triangle is " + area);
+            Console.ReadLine();
+        }
+    }
+}
